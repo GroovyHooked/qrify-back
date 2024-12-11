f63cda06e8b809ef2bb527bf030487229ac11dcf
groovyhooked <dafrenchie2002@yahoo.fr>
2024-12-11 17:10:34 +0100

    Cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
 
 const authRouter = require('./routes/auth')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
